592853549e209375d8563f97e71974c36ba8f136
premalathas1 <premalathas@rvsgroup.com>
2026-09-24 20:40:43 +1200

    Create starter.sql

---
+++ starter.sql
@@ -0,0 +1,21 @@
+-- PL/SQL IF-ELSE: Student Pass or Fail
+-- Complete the program below.
+-- Passing mark: 40
+
+-- The program should:
+-- 1. Store the student's marks in the variable v_marks.
+-- 2. Use an IF-ELSE statement.
+-- 3. Display PASS when marks are >= 40.
+-- 4. Display FAIL when marks are < 40.
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+v_marks NUMBER := 75;
+BEGIN
+
+-- TODO: Write your IF-ELSE statement here.
+
+
+END;
+/
