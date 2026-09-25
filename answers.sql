f8101d5df4985b3bbc9e70742b24be0f605c4d07
premalathas1 <premalathas@rvsgroup.com>
2026-09-25 15:30:34 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,14 +1,1 @@
-SET SERVEROUTPUT ON;
 
-DECLARE
-    marks NUMBER := 75;
-BEGIN
-
-    IF marks >= 50 THEN
-        DBMS_OUTPUT.PUT_LINE('Student has Passed');
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('Student has Failed');
-    END IF;
-
-END;
-/
